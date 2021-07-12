14debcd6249f530485c4e0fa5812f987cdc5017c
jitendradev <jitendra.suthar@growcify.com>
2021-07-12 23:21:11 +0530

    Added handling POSt requests

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
+const todos = require('../backend/routes/todos')
+const cors = require('cors');
 const express = require('express');
 
 //this app represent express application.
 const app = express();
 
+
+
 const mongoose = require('mongoose');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/todos", todos)
 
 //dotenv used to read environment variables from .env file.
 require('dotenv').config()
@@ -16,9 +26,7 @@ app.listen(port, () =>
 })
 
 //const connection_string is assigned connection string to mongoDB connection.
-const connection_string = process.env.CONNETCTION_STRING;
-
-
+const connection_string = process.env.CONNECTION_STRING;
 
 //used mongoose to connect to mongoDB, and passed some object parameters to it.
 
