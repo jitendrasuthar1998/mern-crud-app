7e7d8c679732232ceede47506ba9510775cf7197
jitendradev <jitendra.suthar@growcify.com>
2021-07-12 21:45:15 +0530

    Created server using express and connected mongoDB to server

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+//this app represent express application.
+const app = express();
+
+const mongoose = require('mongoose');
+
+//dotenv used to read environment variables from .env file.
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>
+{
+  console.log(`Listening on Port ${port}`)
+})
+
+//const connection_string is assigned connection string to mongoDB connection.
+const connection_string = process.env.CONNETCTION_STRING;
+
+
+
+//used mongoose to connect to mongoDB, and passed some object parameters to it.
+
+mongoose.connect(connection_string, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+  .then(() =>
+  {
+    //if connection is successful, then show this message in log.
+    console.log("MongoDB connection was successful.")
+  })
+  .catch((error) =>
+  {
+    console.error('MongoDB connection failed', error);
+  })
+
+
+app.get('/', (req, res) =>
+{
+  res.send('Hello World from Jitendra suthar.Learning to create server using Express')
+})
+
